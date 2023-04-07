d8ba62b31d010369c45d3238a0ae5a01b911c283
Calidan <calidan@vip.qq.com>
2023-04-07 11:12:38 +0800

    更新测试用例

--- test/todo-page.test.tsx
+++ test/todo-page.test.tsx
@@ -1,10 +1,24 @@
 import React from "react"
-import { act, render, screen, waitFor } from "@testing-library/react"
+import { act, render, screen } from "@testing-library/react"
 import "@testing-library/jest-dom"
 import { TodoPage } from "../src/pages/todo-page"
+// import { todoApi } from "../src/api/todo"
+
+// beforeAll(() => {
+//   const mockGetList = jest.fn()
+//   mockGetList.mockResolvedValue({
+//     data: [
+//       { id: 1, content: "学习React", isDone: false },
+//       { id: 2, content: "学习Vue", isDone: true }
+//     ]
+//   })
+//   todoApi.getList = mockGetList
+// })
 
 test("测试 Todo 页面", async () => {
-  render(<TodoPage />)
+  await act(async () => {
+    render(<TodoPage />)
+  })
   await screen.findAllByText("我的Todo列表")
   await screen.findByText("学习React")
   const button = await screen.findByText("我知道了")
